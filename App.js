4bacefb4abc721b712cf11a7a7a04cde036091f3
jhlee0214 <keith.kazu@gmail.com>
2025-06-11 09:52:10 +1000

    Try with buttons

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [number, setNumber] = useState(0);
+
+
   return (
     <View style={styles.container}>
-      <Text>STILLL? </Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Result: {number}</Text>
+      <View style={styles.btnGroup}>
+        <Button
+          title="Increment"
+          onPress={() => setNumber(number + 1)}
+        />
+        <Button
+          title="Decrement"
+          onPress={() => setNumber(number - 1)}
+        />
+      </View>
+      <StatusBar barStyle="dark-content" hidden={false} />
     </View>
   );
 }
@@ -17,4 +31,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: 'purple',
+  },
+
+  btnGroup: {
+    flexDirection: 'row',
+    gap: 10,
+  },
+
 });

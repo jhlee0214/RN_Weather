143c3b038287c07456c58be405394d8abc4ca7b6
jhlee0214 <keith.kazu@gmail.com>
2025-06-12 10:41:08 +1000

    basic frame

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button  } from 'react-native';
 import React, { useState } from 'react';
 
-export default function App() {
-  const [number, setNumber] = useState(0);
-
-
+const App = () => {
+  // This is a simple React Native app that uses Expo.
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Result: {number}</Text>
-      <View style={styles.btnGroup}>
-        <Button
-          title="Increment"
-          onPress={() => setNumber(number + 1)}
-        />
-        <Button
-          title="Decrement"
-          onPress={() => setNumber(number - 1)}
-        />
+
+      <View style={styles.cityContainer}>
+        <Text style={styles.city}>Melbourne</Text>
       </View>
-      <StatusBar barStyle="dark-content" hidden={false} />
+
+      <View style={styles.weatherContainer}>
+        <View style={styles.day}>
+          <Text style={styles.regDate}>11/06/2025 Weds 10:26</Text>
+          <Text style={styles.desc}>Weather</Text>
+        </View>
+        <View style={styles.tempContainer}>
+          <Text style={styles.temp}>25</Text>
+        </View>
+      </View>
+
+      <StatusBar style='auto'/>
     </View>
   );
 }
@@ -27,21 +29,67 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#ffe01a',
+
+  },
+
+  cityContainer: {
+    flex: 1,
+  },
+
+  city: {
+    flex: 1,
+    marginTop: 60,
+    paddingTop: 20,
+    fontSize: 40,
     justifyContent: 'center',
-  },
-
-  text: {
-    fontSize: 50,
+    textAlign: 'center',
     fontWeight: 'bold',
-    marginBottom: 20,
-    color: 'purple',
   },
 
-  btnGroup: {
-    flexDirection: 'row',
-    gap: 10,
+  weatherContainer: {
+    flex: 3,
   },
+
+  day: {
+    flex: 0.2,
+    alignItems: 'center',
+  },
+
+  regDate: { 
+    paddingTop: 10,
+    paddingRight: 20,
+    paddingLeft: 20,
+    paddingBottom: 15,
+    alignItems: 'center',
+    backgroundColor: 'black',
+    color: 'white',
+    fontWeight: 'bold',
+    borderRadius: 20,
+  },
+
+  desc: {
+    flex: 1.5,
+    alignItems: 'center',
+    marginTop: 20,
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+
+  tempContainer: {
+    flex: 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  temp: {
+    justifyContent: 'center',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 120,
+  }
+
 
 });
+
+export default App;
